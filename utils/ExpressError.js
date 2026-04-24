46637934fa0d98782fbccb2d3ae7e7079afb4d07
Shivu30-pal <220490131044.shivam@gmail.com>
2026-04-24 14:33:49 +0530

    Update ExpressError.js

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,1 +1,9 @@
+class ExpressError extends Error{
+    constructor(statuscode,message){
+        super();
+        this.statuscode = statuscode;
+        this.message = message;
+    }
+}
 
+module.exports = ExpressError;
